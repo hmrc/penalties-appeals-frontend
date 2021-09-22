33a6543797ac3d3e8e240cb4d14049105904718c
Lewis Mitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-09-22 11:51:58 +0100

    [PRM-657] - Updated content on file upload page (#145)

--- test/connectors/UpscanInitiateConnectorSpec.scala
+++ test/connectors/UpscanInitiateConnectorSpec.scala
@@ -18,15 +18,8 @@ package connectors
 
 import base.SpecBase
 import config.AppConfig
-import connectors.httpParsers.UpscanInitiateHttpParser.{
-  BadRequest,
-  UpscanInitiateResponse
-}
-import models.upscan.{
-  UploadFormTemplateRequest,
-  UpscanInitiateRequest,
-  UpscanInitiateResponseModel
-}
+import connectors.httpParsers.UpscanInitiateHttpParser.{BadRequest, UpscanInitiateResponse}
+import models.upscan.{UploadFormTemplateRequest, UpscanInitiateRequest, UpscanInitiateResponseModel}
 import org.mockito.ArgumentMatchers.any
 import org.mockito.Mockito.{when, _}
 import play.api.test.Helpers._
