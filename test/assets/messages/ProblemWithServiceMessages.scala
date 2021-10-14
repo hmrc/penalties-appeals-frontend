e907825f9df9d15eabed6b5871ffeb8743b79413
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2021-10-14 16:00:49 +0100

    added Problem with service page and tests

---
+++ test/assets/messages/ProblemWithServiceMessages.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package messages
+
+object ProblemWithServiceMessages {
+
+  val title = "Sorry, there is a problem with the service - Appeal a VAT penalty - GOV.UK"
+
+  val heading = "Sorry, there is a problem with the service"
+
+  val p1 = "We have not saved your answers."
+
+  val p2 = "You will be able to use the service later."
+
+  val p3 = "If you prefer, you can appeal by letter. Write to:"
+
+  val p4 = "Solicitor‘s Office and Legal Services HMRC BX9 1ZT"
+}
---
+++ test/controllers/ProblemWithServiceControllerSpec.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers
+
+import base.SpecBase
+import org.mockito.ArgumentMatchers.any
+import org.mockito.Mockito.{reset, when}
+import play.api.mvc.Result
+import play.api.test.Helpers._
+import testUtils.AuthTestModels
+import uk.gov.hmrc.auth.core.retrieve.{ItmpAddress, Name, Retrieval, ~}
+import uk.gov.hmrc.auth.core.{AffinityGroup, Enrolments}
+import uk.gov.hmrc.http.ServiceUnavailableException
+import views.html.errors.ProblemWithServicePage
+
+import scala.concurrent.Future
+
+class ProblemWithServiceControllerSpec extends SpecBase {
+
+  val problemWithServicePage = injector.instanceOf[ProblemWithServicePage]
+
+  class Setup(authResult: Future[~[~[~[~[Option[AffinityGroup], Enrolments], Option[Name]], Option[String]], Option[ItmpAddress]]]) {
+
+    reset(mockAuthConnector)
+    when(mockAuthConnector.authorise[~[~[~[~[Option[AffinityGroup], Enrolments], Option[Name]], Option[String]], Option[ItmpAddress]]](
+      any(), any[Retrieval[~[~[~[~[Option[AffinityGroup], Enrolments], Option[Name]], Option[String]], Option[ItmpAddress]]]]())(
+      any(), any())
+    ).thenReturn(authResult)
+
+    val controller: ProblemWithServiceController = new ProblemWithServiceController(
+      problemWithServicePage
+    )(mcc, appConfig, authPredicate)
+  }
+
+  "onPageLoad" should {
+    "return the 500 status code" in new Setup(AuthTestModels.successfulAuthResult) {
+      val result: Future[Result] = controller.onPageLoad()(fakeRequest)
+      status(result) shouldBe INTERNAL_SERVER_ERROR
+    }
+
+    "the user is unauthorised" when {
+      "return 403 (FORBIDDEN) when user has no enrolments" in new Setup(AuthTestModels.failedAuthResultNoEnrolments) {
+        val result: Future[Result] = controller.onPageLoad()(fakeRequest)
+        status(result) shouldBe FORBIDDEN
+      }
+
+      "return 303 (SEE_OTHER) when user can not be authorised" in new Setup(AuthTestModels.failedAuthResultUnauthorised) {
+        val result: Future[Result] = controller.onPageLoad()(fakeRequest)
+        status(result) shouldBe SEE_OTHER
+      }
+    }
+  }
+
+}
