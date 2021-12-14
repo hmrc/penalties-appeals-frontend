d89bc1fa85fc1513e9dc6e70b52382b88ff2a0cf
JBlacklawsTen10 <58850837+JBlacklaws@users.noreply.github.com>
2021-12-14 14:18:29 +0000

    [PRM-885] Removed flexmark-all

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -22,7 +22,6 @@ object AppDependencies {
     "org.jsoup"               %  "jsoup"                    % "1.13.1"           % "test, it",
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"            % "test, it",
-    "com.vladsch.flexmark"     %  "flexmark-all"              % "0.36.8"           % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.26.3"           % "it",
     "uk.gov.hmrc"             %% "service-integration-test" % "1.2.0-play-28"    % "test, it",
     "org.mockito"             % "mockito-core"              % "3.4.6"            % "test, it"
