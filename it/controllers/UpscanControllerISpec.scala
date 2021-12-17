95c98c2e95d2b61fbf760d731c907d5b9e2cf408
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2021-12-17 11:16:48 +0000

    [PRM-804] - Bug fixing on JS upload

--- it/controllers/UpscanControllerISpec.scala
+++ it/controllers/UpscanControllerISpec.scala
@@ -385,7 +385,7 @@ class UpscanControllerISpec extends IntegrationSpecCommonBase {
       status(result) shouldBe BAD_REQUEST
     }
 
-    "return 204 (No Content) when the form binds and update the record in Mongo (if after 1 second since last update)" in new Setup {
+    "return 204 (No Content) when the form binds and update the record in Mongo (if previous upload is in FAILED state)" in new Setup {
       await(repository.collection.countDocuments().toFuture()) shouldBe 0
       await(repository.updateStateOfFileUpload("1234",
         UploadJourney(
@@ -396,26 +396,21 @@ class UpscanControllerISpec extends IntegrationSpecCommonBase {
             "some.message"
           ))
         ), isInitiateCall = true))
-      Thread.sleep(2000)
       val result: Future[Result] = controller.filePosted("1234")(FakeRequest("GET", "/file-posted?key=ref2"))
       status(result) shouldBe NO_CONTENT
       await(repository.getStatusOfFileUpload("1234", "ref2")).get.status shouldBe UploadStatusEnum.WAITING.toString
     }
 
-    "return 204 (No Content) when the form binds and update the record in Mongo (if before 1 second since last update)" in new Setup {
+    "return 204 (No Content) when the form binds and update the record in Mongo (if file was not in FAILED state)" in new Setup {
       await(repository.collection.countDocuments().toFuture()) shouldBe 0
       await(repository.updateStateOfFileUpload("1234",
         UploadJourney(
           reference = "ref2",
-          fileStatus = UploadStatusEnum.FAILED,
-          failureDetails = Some(FailureDetails(
-            FailureReasonEnum.REJECTED,
-            "some.message"
-          ))
+          fileStatus = UploadStatusEnum.WAITING
         ), isInitiateCall = true))
       val result: Future[Result] = controller.filePosted("1234")(FakeRequest("GET", "/file-posted?key=ref2"))
       status(result) shouldBe NO_CONTENT
-      await(repository.getStatusOfFileUpload("1234", "ref2")).get.status shouldBe FailureReasonEnum.REJECTED.toString
+      await(repository.getStatusOfFileUpload("1234", "ref2")).get.status shouldBe UploadStatusEnum.WAITING.toString
     }
   }
 
