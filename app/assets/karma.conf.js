740fa59d37603083b3ea47c04c29978bb0d177f3
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-01-27 10:57:38 +0000

    [PRM-958] - CI test

--- app/assets/karma.conf.js
+++ app/assets/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
         specReporter: {
             suppressSkipped: true
         },
-        port: 4444,
+        port: 9876,
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: true,
