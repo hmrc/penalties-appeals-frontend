887ec2f5cc44aa18438e485a96637bf6e7ad5bde
AndrewRButcher <6122384+AndrewRButcher@users.noreply.github.com>
2022-07-25 10:36:36 +0100

    PRM-1610 upgrade hmrc-mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,6 +1,5 @@
 import play.core.PlayVersion
 import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
@@ -10,13 +9,13 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.21.0-play-28",
     "uk.gov.hmrc"             %% "play-frontend-govuk"            % "2.0.0-play-28",
     "uk.gov.hmrc"             %% "play-language"                  % "5.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.64.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.68.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.11.0-play-28"
   )
 
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.24.0"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.64.0"           % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.68.0"           % "test, it",
     "org.scalatest"           %% "scalatest"                % "3.2.12"            % "test, it",
     "org.jsoup"               %  "jsoup"                    % "1.15.1"           % "test, it",
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % "test, it",
