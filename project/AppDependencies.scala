80a6780cd579d125d03afe61da9362a2b97e5cc3
Lewis Mitchell <53828896+LewisMitchell@users.noreply.github.com>
2022-09-08 14:06:20 +0100

    [PRM-1750] - Java 11 upgrade (#373)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,17 +5,17 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "5.24.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.21.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.2.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.24.0-play-28",
     "uk.gov.hmrc"             %% "play-frontend-govuk"            % "2.0.0-play-28",
     "uk.gov.hmrc"             %% "play-language"                  % "5.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.68.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.71.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.11.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.24.0"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.68.0"           % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.2.0"            % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.71.0"           % "test, it",
     "org.scalatest"           %% "scalatest"                % "3.2.12"            % "test, it",
     "org.jsoup"               %  "jsoup"                    % "1.15.1"           % "test, it",
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % "test, it",
