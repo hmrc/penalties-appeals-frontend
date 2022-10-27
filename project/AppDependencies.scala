e2eef566e8793d40ff338b8b8c962c2c099773b2
“BackRowOla” <56401572+BackRowOla@users.noreply.github.com>
2022-10-27 16:35:34 +0100

    updated scala version to 2.13 & other dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,24 +5,23 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.2.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.24.0-play-28",
-    "uk.gov.hmrc"             %% "play-frontend-govuk"            % "2.0.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.8.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "3.32.0-play-28",
     "uk.gov.hmrc"             %% "play-language"                  % "5.2.0-play-28",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.71.0",
-    "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.11.0-play-28"
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.73.0",
+    "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.12.0-play-28"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.2.0"            % "test, it",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.71.0"           % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.8.0"            % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.73.0"           % "test, it",
     "org.scalatest"           %% "scalatest"                % "3.2.12"            % "test, it",
-    "org.jsoup"               %  "jsoup"                    % "1.15.1"           % "test, it",
+    "org.jsoup"               %  "jsoup"                    % "1.15.3"           % "test, it",
     "com.typesafe.play"       %% "play-test"                % PlayVersion.current  % "test, it",
     "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"            % "test, it",
-    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.26.3"           % "it",
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.33.2"           % "it",
     "uk.gov.hmrc"             %% "service-integration-test" % "1.3.0-play-28"    % "test, it",
-    "org.mockito"             % "mockito-core"              % "4.6.0"            % "test, it",
+    "org.mockito"             % "mockito-core"              % "4.8.0"            % "test, it",
     "com.vladsch.flexmark"   % "flexmark-all"               % "0.62.2"           % "test, it"
   )
 }
