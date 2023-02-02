9f59c74306ba221b5921efbf289149775c5b06d5
JBlacklaws <58850837+JBlacklaws@users.noreply.github.com>
2023-02-02 15:47:01 +0000

    [PRM-2198] Updated test depedencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.8.0",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28"     % "7.13.0",
     "uk.gov.hmrc"             %% "play-frontend-hmrc"             % "6.3.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"             % "0.73.0",
     "uk.gov.hmrc"             %% "play-conditional-form-mapping"  % "1.12.0-play-28",
@@ -13,7 +13,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.8.0"            % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "7.13.0"            % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.73.0"           % "test, it",
     "org.scalatest"           %% "scalatest"                % "3.2.12"            % "test, it",
     "org.jsoup"               %  "jsoup"                    % "1.15.3"           % "test, it",
