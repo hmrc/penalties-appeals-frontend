a6a698d39324153fe7a4c98fc50c16a050e2d320
roheelarshad <48284720+RoheelArshad@users.noreply.github.com>
2023-05-22 15:10:28 +0100

    PRM-2560 added eventually to tests that fail sporadically

--- it/controllers/UpscanControllerISpec.scala
+++ it/controllers/UpscanControllerISpec.scala
@@ -31,6 +31,7 @@ import utils.{IntegrationSpecCommonBase, SessionKeys}
 import java.time.LocalDateTime
 
 import config.featureSwitches.{FeatureSwitching, WarnForDuplicateFiles}
+import org.scalatest.concurrent.Eventually.eventually
 
 import scala.concurrent.Future
 
@@ -236,8 +237,10 @@ class UpscanControllerISpec extends IntegrationSpecCommonBase with FeatureSwitch
         ), isInitiateCall = true))
       await(repository.getNumberOfDocumentsForJourneyId("1234")) shouldBe 2
       val result: Future[Result] = controller.removeFile("1234", "ref1")(FakeRequest())
-      status(result) shouldBe NO_CONTENT
-      await(repository.getNumberOfDocumentsForJourneyId("1234")) shouldBe 1
+      eventually {
+        status(result) shouldBe NO_CONTENT
+        await(repository.getNumberOfDocumentsForJourneyId("1234")) shouldBe 1
+      }
     }
 
     "return 204 (No Content) when the user has specified a journey ID is NOT in the database" in new Setup {
