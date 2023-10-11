82f59108b5d790986c276bb84a2cdd7838b02544
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2023-10-11 13:47:56 +0100

    [PRM-2976] - Address PR comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -12,7 +12,6 @@ object AppDependencies {
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28"     % bootstrapVersion,
     "uk.gov.hmrc"                  %% "play-frontend-hmrc"             % "7.23.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"             % mongoPlayVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"           % "2.13.3",
     "uk.gov.hmrc"                  %% "crypto-json-play-28"            % "7.3.0"
   )
 
@@ -20,6 +19,6 @@ object AppDependencies {
     "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapVersion    % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % mongoPlayVersion    % "test, it",
     "org.jsoup"               %  "jsoup"                    % "1.16.1"            % "test, it",
-    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.14"           % "it, test"
+    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.27"           % "it, test"
   )
 }
