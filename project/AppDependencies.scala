dfaa1bac913b88cb17c377d79a806d30edd7d9d8
LewisMitchell <53828896+LewisMitchell@users.noreply.github.com>
2023-10-11 11:03:55 +0100

    [PRM-2976] - Uplift dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -10,7 +10,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc"             % "7.20.0-play-28",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc"             % "7.23.0-play-28",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-28"             % mongoPlayVersion,
     "com.fasterxml.jackson.module" %% "jackson-module-scala"           % "2.13.3",
     "uk.gov.hmrc"                  %% "crypto-json-play-28"            % "7.3.0"
