65175018ed9bc7f6fa88a56970d1dc9f34042ead
Prem-Dasari <176310925+Prem-Dasari@users.noreply.github.com>
2025-08-12 14:22:49 +0100

    Fixes

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,14 +1,14 @@
-import sbt._
+import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "8.6.0"
+  val bootstrapVersion = "10.1.0"
 
   val mongoPlayVersion = "2.7.0"
 
   val compile = Seq(
     "uk.gov.hmrc"                  %% "bootstrap-frontend-play-30"     % bootstrapVersion,
-    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"     % "12.8.0",
+    "uk.gov.hmrc"                  %% "play-frontend-hmrc-play-30"     % "8.5.0",
     "uk.gov.hmrc.mongo"            %% "hmrc-mongo-play-30"             % mongoPlayVersion,
     "uk.gov.hmrc"                  %% "crypto-json-play-30"            % "7.6.0"
   )
@@ -16,7 +16,7 @@ object AppDependencies {
   val test = Seq(
     "uk.gov.hmrc"             %% "bootstrap-test-play-30"   % bootstrapVersion    % "test, it",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30"  % mongoPlayVersion    % "test, it",
-    "org.jsoup"               %  "jsoup"                    % "1.21.1"            % "test, it",
+    "org.jsoup"               %  "jsoup"                    % "1.17.1"            % "test, it",
     "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.30"           % "it, test"
   )
 }
