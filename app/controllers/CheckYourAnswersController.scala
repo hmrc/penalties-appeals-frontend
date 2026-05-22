85fd972b387e29186cfbfedc51f248fc84c8ee86
Ali Shah <ali.shah@digital.hmrc.gov.uk>
2026-05-22 12:20:02 +0100

    Refactored SessionAnswersHelper to improve null safety and better error handling

--- app/controllers/CheckYourAnswersController.scala
+++ app/controllers/CheckYourAnswersController.scala
@@ -53,73 +53,67 @@ class CheckYourAnswersController @Inject()(checkYourAnswersPage: CheckYourAnswer
   val pageMode: Mode => PageMode = (mode: Mode) => PageMode(CheckYourAnswersPage, mode)
 
   def onPageLoad: Action[AnyContent] = (authorise andThen dataRetrieval andThen dataRequired).async {
-    implicit userRequest => {
-      if(isLateAppealHelper.isAppealLate() && userRequest.answers.getAnswer[String](SessionKeys.lateAppealReason).isEmpty){
+    implicit userRequest =>
+      if (isLateAppealHelper.isAppealLate() && userRequest.answers.getAnswer[String](SessionKeys.lateAppealReason).isEmpty) {
         logger.warn("[CheckYourAnswersController][onPageLoad] User tried skipping late appeal page, redirecting back to late appeal page")
-        Future(Redirect(controllers.routes.MakingALateAppealController.onPageLoad()))
+        Future.successful(Redirect(controllers.routes.MakingALateAppealController.onPageLoad()))
       } else {
-        userRequest.answers.getAnswer[String](SessionKeys.reasonableExcuse).fold({
+        userRequest.answers.getAnswer[String](SessionKeys.reasonableExcuse) match {
+          case None =>
             logger.error("[CheckYourAnswersController][onPageLoad] User hasn't selected reasonable excuse option - no key in session")
-            Future(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
-        })(
-          reasonableExcuse => {
-            if (sessionAnswersHelper.getAllTheContentForCheckYourAnswersPage().nonEmpty) {
-              logger.debug(s"[CheckYourAnswersController][onPageLoad] Loading check your answers page")
-              for {
-                content <- sessionAnswersHelper.getContentWithExistingUploadFileNames(reasonableExcuse)
-              } yield {
-                Ok(checkYourAnswersPage(content, pageMode(NormalMode))).removingFromSession(SessionKeys.originatingChangePage)
-              }
-            } else {
-              logger.error(s"[CheckYourAnswersController][onPageLoad] User hasn't got all keys in session for reasonable excuse: $reasonableExcuse")
-              logger.debug(s"[CheckYourAnswersController][onPageLoad] User has keys: ${userRequest.session.data} " +
-                s"and tried to load page with reasonable excuse: $reasonableExcuse")
-              Future(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
+            Future.successful(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
+
+          case Some(reasonableExcuse) if sessionAnswersHelper.getAllTheContentForCheckYourAnswersPage().nonEmpty =>
+            logger.debug(s"[CheckYourAnswersController][onPageLoad] Loading check your answers page")
+            sessionAnswersHelper.getContentWithExistingUploadFileNames(reasonableExcuse).map { content =>
+              Ok(checkYourAnswersPage(content, pageMode(NormalMode))).removingFromSession(SessionKeys.originatingChangePage)
             }
-          }
-        )
+
+          case Some(reasonableExcuse) =>
+            logger.error(s"[CheckYourAnswersController][onPageLoad] User hasn't got all keys in session for reasonable excuse: $reasonableExcuse")
+            logger.debug(s"[CheckYourAnswersController][onPageLoad] User has keys: ${userRequest.session.data} " +
+              s"and tried to load page with reasonable excuse: $reasonableExcuse")
+            Future.successful(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
+        }
       }
-    }
   }
 
   def onSubmit(): Action[AnyContent] = (authorise andThen dataRetrieval andThen dataRequired).async {
-    implicit userRequest => {
-      userRequest.answers.getAnswer[String](SessionKeys.reasonableExcuse).fold({
+    implicit userRequest =>
+      userRequest.answers.getAnswer[String](SessionKeys.reasonableExcuse) match {
+        case None =>
           logger.error("[CheckYourAnswersController][onSubmit] No reasonable excuse selection found in session")
-          Future(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
-      })(
-        reasonableExcuse => {
-          if (sessionAnswersHelper.isAllAnswerPresentForReasonableExcuse(reasonableExcuse)) {
-            logger.debug(s"[CheckYourAnswersController][onPageLoad] All keys are present for reasonable excuse: $reasonableExcuse")
-            handleAppealSubmission(reasonableExcuse)
-          } else {
-            logger.error(s"[CheckYourAnswersController][onSubmit] User did not have all answers for reasonable excuse: $reasonableExcuse")
-            Future(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
-          }
-        }
-      )
-    }
+          Future.successful(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
+
+        case Some(reasonableExcuse) if sessionAnswersHelper.isAllAnswerPresentForReasonableExcuse(reasonableExcuse) =>
+          logger.debug(s"[CheckYourAnswersController][onPageLoad] All keys are present for reasonable excuse: $reasonableExcuse")
+          handleAppealSubmission(reasonableExcuse)
+
+        case Some(reasonableExcuse) =>
+          logger.error(s"[CheckYourAnswersController][onSubmit] User did not have all answers for reasonable excuse: $reasonableExcuse")
+          Future.successful(Redirect(controllers.routes.IncompleteSessionDataController.onPageLoad()))
+      }
   }
 
   private def handleAppealSubmission(reasonableExcuse: String)(implicit userRequest: UserRequest[_]): Future[Result] = {
-    appealService.submitAppeal(reasonableExcuse).flatMap(_.fold(
-      {
-        case SERVICE_UNAVAILABLE => Future(Redirect(controllers.routes.ServiceUnavailableController.onPageLoad()))
-        case INTERNAL_SERVER_ERROR | BAD_REQUEST | UNPROCESSABLE_ENTITY => Future(Redirect(controllers.routes.ProblemWithServiceController.onPageLoad()))
-        case CONFLICT => Future(Redirect(controllers.routes.DuplicateAppealController.onPageLoad()))
-        case _ => Future(errorHandler.showInternalServerError(Some(userRequest)))
-      },
-      _ => {
+    appealService.submitAppeal(reasonableExcuse).map {
+      case Left(SERVICE_UNAVAILABLE) =>
+        Redirect(controllers.routes.ServiceUnavailableController.onPageLoad())
+      case Left(INTERNAL_SERVER_ERROR | BAD_REQUEST | UNPROCESSABLE_ENTITY) =>
+        Redirect(controllers.routes.ProblemWithServiceController.onPageLoad())
+      case Left(CONFLICT) =>
+        Redirect(controllers.routes.DuplicateAppealController.onPageLoad())
+      case Left(_) =>
+        errorHandler.showInternalServerError(Some(userRequest))
+      case Right(_) =>
         // Remove any previous appeal data from Mongo + file uploads (if they exist) - fire and forget
         // If it fails it will be picked up by the TTL index
         appealService.removePreviouslySubmittedAppealData(userRequest.session.get(SessionKeys.previouslySubmittedJourneyId))
-        val previouslySubmittedJourneyId = userRequest.answers.journeyId
-        Future(Redirect(controllers.routes.AppealConfirmationController.onPageLoad()).addingToSession(
-          SessionKeys.previouslySubmittedJourneyId -> previouslySubmittedJourneyId,
+        Redirect(controllers.routes.AppealConfirmationController.onPageLoad()).addingToSession(
+          SessionKeys.previouslySubmittedJourneyId -> userRequest.answers.journeyId,
           SessionKeys.penaltiesHasSeenConfirmationPage -> "true"
-        ))
-      }
-    ))
+        )
+    }
   }
 
   def changeAnswer(continueUrl: RedirectUrl, pageName: String): Action[AnyContent] = (authorise andThen dataRetrieval andThen dataRequired) {
